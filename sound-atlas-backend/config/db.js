beb99d8b5dc556ab293f3e2d032272176ad2c3e2
Chris <2739361+chris-aqui@users.noreply.github.com>
2025-01-10 14:44:43 -0500

    init commit for backend code

---
+++ sound-atlas-backend/config/db.js
@@ -0,0 +1,14 @@
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+export const connectDB = async () => {
+	try {
+		const conn = await mongoose.connect(process.env.MongoDB_URI);
+		console.log(`MongoDB connected: ${conn.connection.host} 🌌`);
+	} catch (error) {
+		console.error(`MongoDB Error: ${error.message}`);
+		process.exit(1);
+	}
+};
