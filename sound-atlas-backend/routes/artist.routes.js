19db28c73d514f211ae873fa831eada99d096e91
Chris <2739361+chris-aqui@users.noreply.github.com>
2025-01-13 15:04:50 -0500

    clean up

--- sound-atlas-backend/routes/artist.routes.js
+++ sound-atlas-backend/routes/artist.routes.js
@@ -4,43 +4,6 @@ import { getTopArtists, getArtistDetails } from '../controllers/artist.controlle
 const router = express.Router();
 
 // Artist-related routes
-
-/**
- * @swagger
- * /top-artist:
- *   get:
- *     summary: Get top artists
- *     tags: [Artists]
- *     parameters:
- *       - in: query
- *         name: country
- *         required: true
- *         schema:
- *           type: string
- *         description: Country of the artist
- *       - in: query
- *         name: year
- *         schema:
- *           type: number
- *         description: Release year of the artist
- *     responses:
- *       200:
- *         description: List of top artists
- *         content:
- *           application/json:
- *             schema:
- *               $ref: '#/components/schemas/ApiTopArtistResponse'
- *       400:
- *         description: Bad request
- *         content:
- *           application/json:
- *             schema:
- *               type: object
- *               properties:
- *                 error:
- *                   type: string
- *                   example: "Country parameter is required."
- */
 router.get('/top-artist', getTopArtists);
 router.get('/artist/:id', getArtistDetails);
 
