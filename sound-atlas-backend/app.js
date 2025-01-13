a219087b1e8cdd1d97409b600d02a8f34f36b4de
Chris <2739361+chris-aqui@users.noreply.github.com>
2025-01-13 00:59:06 -0500

    moved path logic to app.js

--- sound-atlas-backend/app.js
+++ sound-atlas-backend/app.js
@@ -1,8 +1,17 @@
 import express from 'express';
+import path from 'path';
 import { errorHandler } from './middlewares/errorHandler.js';
 import artistRoutes from './routes/artist.routes.js';
 
 const app = express();
+
+const __dirname = path.resolve();
+if (process.env.NODE_ENV === 'production') {
+	app.use(express.static(path.join(__dirname, '/sound-atlas-frontend/dist')));
+	app.get('*', (req, res) => {
+		res.sendFile(path.resolve(__dirname, 'sound-atlas-frontend', 'dist', 'index.html'));
+	});
+}
 
 app.use(express.json());
 
