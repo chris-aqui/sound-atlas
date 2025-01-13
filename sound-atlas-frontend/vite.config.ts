cd1584ddef30cbd1a556a59833103253d96d135e
Chris <2739361+chris-aqui@users.noreply.github.com>
2025-01-13 01:18:58 -0500

    adjusted vit config for render

--- sound-atlas-frontend/vite.config.ts
+++ sound-atlas-frontend/vite.config.ts
@@ -1,16 +1,25 @@
 import { defineConfig } from 'vite'
+import { ConfigEnv, UserConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from "path"
 
 // https://vite.dev/config/
-export default defineConfig({
+export default defineConfig(({ command }: ConfigEnv): UserConfig => {
+  const proxyTarget = command === 'serve'
+  ? 'http://localhost:5000'  // Development target
+  : 'https://sound-atlas.onrender.com'  // Production target
+
+
+  return {
   root: './', // Keep the default root for frontend
   envDir: '../', // Specify the directory where the .env file is located
   server: {
     open: true,
     proxy: {
 			"/api": {
-				target: "http://localhost:5000",
+				// target: "http://localhost:5000",
+        target: proxyTarget,
+        changeOrigin: true,
 			},
 		},
   },
@@ -20,4 +29,4 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-})
+}})
