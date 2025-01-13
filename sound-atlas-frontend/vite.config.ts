9611e12af9253880d1e320316576934e7f50cf0b
Chris <2739361+chris-aqui@users.noreply.github.com>
2025-01-13 00:44:42 -0500

    told vite where to find env for render

--- sound-atlas-frontend/vite.config.ts
+++ sound-atlas-frontend/vite.config.ts
@@ -4,6 +4,8 @@ import path from "path"
 
 // https://vite.dev/config/
 export default defineConfig({
+  root: './', // Keep the default root for frontend
+  envDir: '../', // Specify the directory where the .env file is located
   server: {
     open: true,
     proxy: {
